a14d6fe3a2608c4fceaf0b1b49b2daa2335499d3
Anton Shevchuk <antonshevchuk@gmail.com>
2022-12-22 16:15:35 +0100

    Added info about more road types

--- WME-E95.user.js
+++ WME-E95.user.js
@@ -54,15 +54,25 @@
 
   // Road Types
   //   I18n.translations.uk.segment.road_types
+  //   I18n.translations.en.segment.road_types
   const TYPES = {
     street: 1,
     primary: 2,
-    // ...
+    freeway: 3,
+    ramp: 4,
+    trail: 5,
+    major: 6,
+    minor: 7,
     offroad: 8,
-    // ...
+    walkway: 9,
+    boardwalk: 10,
+    ferry: 15,
+    stairway: 16,
     private: 17,
-    // ...
+    railroad: 18,
+    runway: 19,
     parking: 20,
+    narrow: 22
   }
   // Road colors by type
   const COLORS = {
