abac581056ffc18d384c2d3c7cabddbe4b3a9e9c
Anton Shevchuk <antonshevchuk@gmail.com>
2023-01-10 14:40:20 +0100

    Updated version of the dependency

--- WME-E95.user.js
+++ WME-E95.user.js
@@ -15,7 +15,7 @@
 // @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1135567
 // @require      https://greasyfork.org/scripts/452563-wme/code/WME.js?version=1101598
 // @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1129908
-// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1135634
+// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1137009
 // ==/UserScript==
 
 /* jshint esversion: 8 */
@@ -401,7 +401,7 @@
         streetName: addr.street ? addr.street.name : null,
       }
       // options: detect city
-      if (options.detectCity && options.cityName) {
+      if (!address.cityName && options.detectCity && options.cityName) {
         this.log('detected city name "' + options.cityName + '"')
         address.cityName = options.cityName
       }
